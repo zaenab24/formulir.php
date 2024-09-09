9bca85e52a01300a058df2658e1656873148b615
MAKN ENDE 47 <zaenabsira331@gmail.com>
2024-09-09 14:29:51 +0700

    warna

---
+++ nab.php
@@ -0,0 +1,11 @@
+<?php
+
+$color = "red";
+$warna = "blue";
+$sepatu = "black";
+$tas = "pink";
+
+echo "my car is colored " . $color . "<br>";
+echo "my house is colored " . $warna . "<br>";
+echo "my boat is colored " . $sepatu . "<br>";
+echo "my bag is colored " . $tas . "<br>";
